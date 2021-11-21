e4b6361b45f0fdef4b3c88a6dc7f9b846b0829b7
RRRdik <90895036+RRRdik@users.noreply.github.com>
2021-11-21 19:03:17 +0100

    alle opties

--- Vluchtelingen.py
+++ Vluchtelingen.py
@@ -1,1 +1,90 @@
-# nog bezig met de flowchart, zal hier verderwerken als ik alles volledig af heb.
+[1] Je gaat nu vluchten uit Syrië en je hebt net gehoord dat je over een week met een bus naar de grens kan, je had al gepland om nu gelijk met de fiets te gaan, met de fiets duurt het een week om er te komen maar dan moet je zelf alles doen of je gaat met de bus over een week en hoef je maar 2 dagen te reizen. Je gaat nu beslissen of je: 
+A. Met de fiets gaat [2] 
+B. Met de bus gaat [3] 
+
+ 
+
+[2] Je hebt ervoor gekozen om met de fiets omdat je er dan toch sneller bent en ook gelijk weg kan, je gaat nu onderweg en het gaat voor de eerste 4 dagen goed, maar dan toch krijg je een lekke band en kan je niet meer verder op de fiets, je zult nu moeten lopen en je laat je fiets achter. Het lukt je om in de 5e dag alsnog aan de grens te komen en krijgt toegang tot Turkije, je hebt de eerste stap voltooid om veilig te komen in het buitenland maar nu moet je nog erachter komen hoe je verder komt. 
+Ga naar [4] 
+ 
+[3] Je hebt ervoor gekozen om nog een week te wachten om met de bus te gaan zodat je niet te veel energie hoeft te gebruiken en je nog genoeg over houdt om gelijk verder te gaan als je in Turkije komt, je bent net vertrokken en de bus zit heel erg vol, na dag 1 hoor je sirenes afgaan en je hoort allemaal explosies, de bus rijdt zo hard als hij kan om naar de grens te komen zonder geraakt te worden door de explosieven. De bus komt ineens tot stilstand en het wordt gelijk onrustig in de bus, je hoort net dat de benzine op is en je niet verder kan rijden, je hoort een hard geluid, er is iets recht naast de bus geland, je hoort een explosie en dat is het laatste dat je hoort. 
+
+[EINDE 1] Je bent doodgegaan. Slecht einde 
+
+ 
+
+[4] Nu je in Turkije bent moet je uitzoeken hoe je verder kunt gaan, je gaat naar de dichtstbijzijnde stad om te vragen hoe je aan vervoer kan komen en hoe je het beste verder kan. Je komt aan in de stad genaamd Kilis na een dag reizen en je gaat naar het stadhuis om informatie te krijgen, je komt erachter dat je heel goedkoop een fiets kan kopen bij een tweewieler bedrijf in de buurt, je hebt ook een kaart meegekregen met de snelste route naar 2 andere steden waaruit je gemakkelijk de grens over kan, allebei leiden naar een ander land dus je moet nu kiezen naar welk land je gaat. De ene stad is Izmir van waar je naar Griekeland kan gaan via boot, de andere is Edirne waarvan je over land de grens over naar Bulgarije kan. 
+
+Ga je naar: 
+A. Griekeland [5] 
+B. Bulgarije [6] 
+ 
+
+[5] Je gaat via Izmir naar Griekeland, het duurt drie weken om er te komen en tot nu toe ging de reis makkelijk, je komt aan in Izmir en zoekt uit hoe je op een boot kan komen om naar Griekeland te gaan, gelukkig zijn er mensen die Engels kunnen en waar je mee kan praten. Ze hebben je verteld dat er elke dag een boot gaat waarop je een fiets mag meenemen en dat het erg goedkoop is om mee te varen, ze vertellen je dat als je nu vertrekt dat je de boot nog makkelijk kan halen en je gaat nu naar de boot toe. Je komt aan bij de boot en ze maken nergens een probleem van en je kan gelijk mee. 
+Ga naar [7] 
+
+ 
+
+[6] Je gaat naar Bulgarije via Edirne, het duurt 4 weken om er te komen en je besluit om een dag te rusten. Wanneer je weer verdergaat kom je erachter dat de grens streng wordt gecontroleerd omdat Bulgarije geen vluchtelingen in hun land wilt laten. Je komt aan bij de grens en ze vragen je waarom je naar Bulgarije wilt gaan. 
+Je vertelt ze: 
+A. Je komt er omdat je als vluchteling naar Nederland wilt komen [8] 
+B. Je komt er omdat je op vakantie bent en je graag Bulgarije wilt zien [9] 
+
+ 
+
+[7] Je komt aan in Griekeland met de boot na een lange dag varen en als je aankomt wordt er niets gevraagd behalve je paspoort. Ze zien dat je een geldig paspoort hebt en je mag zonder vragen verder gaan, je gaat met de fiets verder net als in Morocco en je vraagt rond hoe je verder kan gaan naar Nederland. Je hebt na een paar dagen vragen gehoord dat je goedkoop tickets kan krijgen voor een vliegtuig waar je vracht mee kan nemen om naar Duitsland te komen, het duurt 2 weken voor je bij het vliegveld kwam maar je mocht gelijk een kaartje kopen en je had nog precies genoeg om nog genoeg over te hebben om in Duitsland verder naar Nederland te gaan. 
+Ga naar [10] 
+
+ 
+
+[8] Ze zeggen dat ze geen vluchtelingen binnen hun land laten zelfs als je er alleen maar doorheen wilt reizen. Je wordt niet het land binnengelaten. 
+[EINDE 2] Je kunt niet meer verder en je kunt ook niet hier blijven. Fout einde. 
+
+ 
+
+[9] Je wordt toegelaten in Bulgarije en je kan weer verder me je reis. Voordat je verder kan moet je nog een paar dagen fietsen naar de dichtstbijzijnde stad want er zijn alleen kleine dorpen in de buurt waar je bijna niks kan krijgen. Wanneer je aankomt bij de stad vraag je rond hoe je het beste naar Nederland kunt gaan, je wordt verteld dat er een bus is die je in twee maanden er kan laten komen voor een goede prijs. Je komt na een paar dagen aan bij de bus maar de bestuurder zegt dat ze geen fietsen mee kunnen nemen, de bus vertrekt over een week dus je besluit de fiets te verkopen voordat je weggaat. Je vertrekt nu op een lange reis met de bus. 
+Ga naar [11] 
+
+ 
+
+[10] Je komt aan in Duitsland met het vliegtuig en je hebt een goede vlucht gehad, je wordt door de douane gevraagd waarom je naar Duitsland bent gekomen en je legt hem uit dat je een vluchteling bent onderweg naar Nederland, hij begrijpt het en laat je zonder problemen verder, je hebt je fiets opgehaald van de afzet plaats en je gaat nu onderweg naar Nederland. 
+Ga naar [12] 
+ 
+[11] Je bent nu ondertussen in Oostenrijk en er willen meer mensen in de bus, de bus zit al helemaal vol en de bestuurder vraagt of mensen weg willen voor de nieuwe reizigers, iedereen heeft nee gezegd maar er wilt een oude vrouw de bus in, het is nu aan jou de keuze. 
+A. Je gaat op een andere manier verder en laat de oude vrouw op de bus [13] 
+B. Je blijft zitten en gaat verder met de bus [14] 
+
+ 
+
+[12] Je wilt net gaan fietsen maar je ziet dat je een lekke band hebt, gelukkig kun je zonder haast naar een fietsenmaker gaan om je band te repareren, nu je band weer heel is kun je verdergaan maar je denkt er ook aan dat je nu niet per se hoeft te haasten aangezien je nu al zo dichtbij bent en geen problemen hebt op het moment, je neemt een paar dagen rust voordat je weer verder gaat met je reis zodat je met goede rust weer met volle energie weer door kan gaan. Je gaat nu weer verder naar Nederland en gaat bedenken waar je heen wilt gaan, je zoekt op waar het vluchtelingenhulp gebouw is en je besluit dat je het beste recht daar naartoe kan gaan, je gaat nu onderweg. 
+Ga naar [15] 
+ 
+[13] Je gaat van de bus af zodat de oude vrouw in de bus kan, je zult nu uit moeten zoeken hoe je weer verder kan gaan, je hebt ook geen fiets meer dus zal je te voet moeten gaan, gelukkig ben je nu in een grote stad en kan je gelijk rondvragen waar je nu heen kan. Je wordt verteld dat je te voet niet buiten de grenzen kan komen en dat je met de fiets er ook niet langs mag, je hebt geen rijbewijs dus je kunt niet een auto gebruiken, je zit nu vast en kunt nergens heen, nu zul je in Oostenrijk moeten verblijven. 
+[EINDE 3] Je kunt nergens meer heen en zal je alleen nog maar in Oostenrijk kunnen blijven, je reis stopt hier en je bent niet gekomen waar je wilde zijn. 
+ 
+[14] Je besluit om te blijven zitten omdat je het niet kan riskeren nu nog problemen te krijgen, je gaat verder tot je in Nederland bent. Je zit nu in Duitsland aangekomen en je hoeft nog maar een paar weken te wachten tot je in Nederland aankomt, de bestuurder geeft aan dat de bus voor drie dagen stil moet staan voor onderhoud maar dat er geen groot probleem is om je zorgen over te maken dus als je over drie dagen terug bent kun je weer verder. Je besluit die drie dagende stad te bekijken en daarna weer terug te gaan naar de bus. 
+Ga naar [16] 
+ 
+[15] Je reis zult drie weken duren voordat je bij de grens komt en dan moet je nog een week nemen om bij vluchtelingenwerk te komen, je bent nu twee weken onderweg en als je aan het fietsen bent rijdt er een auto naast je die zijn raam opendoet, de bestuurder vraagt waar je heen gaat en je vertelt hem dat je opweg bent naar Nederland en hij zegt dat hij er ook heen gaat en je een lift wilt aanbieden. Je accepteert het aanbod en je zet je fiets op de fietsenhanger achterop de auto, je vraagt de bestuurder waarom hij in Duitsland was en hij verteld je dat hij voor zijn werk vluchtelingen helpt in de West-Europa regio. Je vertelt hem dat je zelf een vluchteling bent en hij is verrast, hij vertelt je dat hij net klaar was met zijn vorige vluchteling te helpen in Duitsland en hij daarom jou gelijk hulp kunt aanbieden. 
+Ga naar [17] 
+ 
+[16] Je komt weer terug bij de bus en na een paar uur vertrek je weer, het heeft even geduurd maar je bent nu aangekomen in Nederland, je reis was heel erg lang maar het was het wachten toch waard, je bent er nu. Je zit nu in Gelderland en je stapt de bus uit, je vraagt wat rond en je wordt verteld dat er een vluchtelingenwerk gebouw dichtbij in de buurt zit die je kan helpen. Je neemt een dag rust en je gaat ernaartoe. Wanneer je aankomt vragen ze om je afkomst en reden van immigratie. 
+Je vertelt je reden: 
+A. Je bent gevlucht omdat je in een oorlogsgebied zat en het niet meer veilig was [18] 
+B. Je bent gevlucht omdat je niet meer in Syrië kon wonen omdat je onderdrukt werd door de tyrant [19] 
+ 
+[17] Je komt aan in Nederland en je hebt tijdens de rit alles verteld dat er is gebeurd, de man vertelt je dat alles goed zal komen en hij je gelijk kan helpen wanneer je aankomt. Hij brengt je naar zijn kantoor waar hij al zijn papierwerk voor vluchtelingen doet en ook gesprekken voert maar omdat hij alles al van je weet door de lange reis kan hij gelijk met het papierwerk aan de gang, hij vertelt je al gelijk dat je gegarandeerd toestemming kan krijgen tot verblijf en het niet moeilijk zal zijn voor je om een verblijfsvergunning te krijgen, je bent erg blij om dit te horen en jullie gaan samen aan de gang om alles te regelen. 
+Ga naar [20] 
+ 
+[18] Ze begrijpen wat er is gebeurd en ze zullen je helpen om in Nederland goed terecht te komen, ze vragen nog een paar dingen en ze gaan met je aan de gang. Al het papierwerk dat nu gedaan kon worden is klaar en je moet nu een tijdje wachten op antwoord hoe het is verlopen, er is een grote kans dat je wordt toegelaten en mag verblijven dus kan je nu rustig de tijd nemen om te wachten, je wordt voor nu verwezen naar een verblijfplaats waar je kunt rusten en eten krijgen om te wachten op resultaten.  
+Ga naar [21] 
+
+[19] Ze vertellen je dat er geen goede reden is om voor een tyrant te vluchten en ze vertellen je dat je terug moet gaan omdat het niet een wettelijke reden is om daarvoor te vluchten omdat het polities is en dus kunnen ze je geen vluchtelingenhulp aanbieden. 
+[Einde 4] Je moet weer terug en je hele reis was voor niets. Slecht einde. 
+ 
+[20] Je hebt te horen gekregen dat je langs moet komen naar vluchtelingenwerk dus je gaat erheen. De man vertelt je dat je toestemming hebt gekregen voor een verblijfsvergunning en je nu in Nederland mag blijven, hij zegt dat je nog hulp van hem kan krijgen als je dat nodig hebt voor het zoeken naar een baan of andere zaken wanneer je maar wilt dus je mag gewoon bij hem langskomen als je het nodig hebt. Je bent erg blij met het resultaat van je reis en je hebt nu eindelijk zekerheid dus kun je nu een goed leven gaan leiden. 
+[Einde 5] Je bent nu succesvol in Nederland aangekomen en je mag blijven. Goed einde. 
+ 
+[21] Na een paar weken krijg je bericht om weer langs te komen, wanneer je aankomt staan ze je op te wachten, ze vertellen je dat ze goed nieuws hebben. Je bent verteld dat alles goed is gegaan en dat je verblijfsvergunning er snel aankomt, ze vertellen je ook dat je terug kan komen voor ander papierwerk of hulp die je nodig kunt hebben en daar ben je erg blij mee, je hebt nu eindelijk een garantie in het leven en je kunt nu veilig en wel een goed leven beginnen. 
+
+[Einde 6] Je bent na een lange reis met de bus in Nederland aangekomen en je kunt nu veilig leven. Goed einde. 
